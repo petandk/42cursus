68936a8c1fc70a344477927716ac84c0f9bb5526
Raúl Manzanas Trillo <rmanzana@student.42barcelona.com>
2025-10-20 19:24:18 +0200

    chekiando00

--- 4 cuarto_circulo/C++/cpp01/ex02/src/main.cpp
+++ 4 cuarto_circulo/C++/cpp01/ex02/src/main.cpp
@@ -6,12 +6,19 @@
 /*   By: rmanzana <rmanzana@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 20:46:53 by rmanzana          #+#    #+#             */
-/*   Updated: 2025/09/16 18:15:49 by rmanzana         ###   ########.fr       */
+/*   Updated: 2025/10/20 19:14:22 by rmanzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+
+/*
+	brain: main var;
+	*stringPTR: POINTER to that var; just a "paper" with the address written in
+	&stringREF: ALIAS to that var; same var, different name
+	Use & for address, * to dereference pointer.
+*/
 
 int main(void)
 {
