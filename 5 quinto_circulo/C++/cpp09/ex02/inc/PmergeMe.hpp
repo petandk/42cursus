d90a51fff5b9f4d7d91c91c8bf112617799652a0
Raúl Manzanas Trillo <rmanzana@student.42barcelona.com>
2026-06-06 21:47:30 +0200

    cpp9x02 started fj

--- 5 quinto_circulo/C++/cpp09/ex02/inc/PmergeMe.hpp
+++ 5 quinto_circulo/C++/cpp09/ex02/inc/PmergeMe.hpp
@@ -13,6 +13,19 @@ class PmergeMe{
         std::vector<std::pair<unsigned int, int> > _valuesV;
         std::deque<std::pair<unsigned int, int> > _valuesD;
 
+        std::vector<int> jacobsthal(int prev);
+        //vector
+        std::vector<std::pair<unsigned int, int> > sortVector(std::vector<std::pair<unsigned int, int> > &vec);
+        void fightVector(std::vector<std::pair<unsigned int, int> > &fighters, 
+                        std::vector<std::pair<unsigned int, int> > &winers,
+                        std::vector<std::pair<unsigned int, int> > &losers);
+        void    jacobsthalVector(std::vector<std::pair<unsigned int, int> > &winers,
+                                std::vector<std::pair<unsigned int, int> > &losers);
+        std::vector<int> jacobsthalVectorSeq(int n);
+        
+        //deque
+        std::deque<std::pair<unsigned int, int> > sortDeque(std::deque<std::pair<unsigned int, int> > &dec);
+
     public:
         PmergeMe();
         PmergeMe(const PmergeMe &other);
@@ -24,9 +37,7 @@ class PmergeMe{
         #ifdef DEBUG
             void debugValues(void);
         #endif
-        bool sortVector(std::vector<std::pair<unsigned int, int> > &vec);
-        bool sortDeque(std::deque<std::pair<unsigned int, int> > &dec);
-        
+                
         void FordJohnson(void);
 };
 
--- 5 quinto_circulo/C++/cpp09/ex02/src/PmergeMe.cpp
+++ 5 quinto_circulo/C++/cpp09/ex02/src/PmergeMe.cpp
@@ -56,15 +56,120 @@ bool PmergeMe::readValues(int argc, char *argv[])
         return (false);
     }
 }
+/*
+    For a vector like [1, 2, 3]
+    The loop checks elements in pairs:
+    first loop  -> (1, 2)
+    second loop -> (3, end)
 
-static bool tournament()
+    If next == fighters.end(), it means there is one element left
+    (odd amount of numbers), so we put that last one in losers.
+
+    Iterator basics:
+    ++it      moves to the next element
+    *it       gets the element
+    it->first gets the first value in the pair
+    it->second gets the second value in the pair
+*/
+void PmergeMe::fightVector(std::vector<std::pair<unsigned int, int> > &fighters, 
+                            std::vector<std::pair<unsigned int, int> > &winers,
+                            std::vector<std::pair<unsigned int, int> > &losers)
+{   
+    winers.clear();
+    losers.clear();
+    std::vector<std::pair<unsigned int, int> >::iterator fg = fighters.begin();
+    while (fg != fighters.end())
+    {
+        std::vector<std::pair<unsigned int, int> >::iterator next = fg;
+        ++next;
+        if (next == fighters.end())
+        {
+            losers.push_back(*fg);
+            break;
+        }
+
+        if (fg->first > next->first)
+        {
+            winers.push_back(*fg);
+            losers.push_back(*next);
+        }
+        else
+        {
+            winers.push_back(*next);
+            losers.push_back(*fg);
+        }
+        fg = next;
+        ++fg;
+    }
+}
+
+std::vector<int> PmergeMe::jacobsthal(int size)
+{
+    std::vector<int> retSeq;
+
+    if (size <= 0)
+        return (retSeq);
+
+    retSeq.push_back(1);
+    if (size == 1)
+        return (retSeq);
+
+    int prev1 = 1;
+    int prev2 = 1;
+    int next = 3;
+
+    while (next <= size)
+    {
+        retSeq.push_back(next);
+        prev2 = prev1;
+        prev1 = next;
+        next = prev1 + 2 * prev2;
+    }
+    return (retSeq);
+}
+
+std::vector<int> PmergeMe::jacobsthalVectorSeq(int n)
+{
+    std::vector<int> retSeq;
+    int jacob = 1;
+    if (n <= 0)
+        return (retSeq);
+    retSeq.push_back(jacob);
+    if (n == 1)
+        return (retSeq);
+    while (jacob <= n && retSeq.size() <= n)
+    {
+         //jacob = nextJacobsthal(retSeq.size() + 1);
+    }
+
+}
+
+void PmergeMe::jacobsthalVector(std::vector<std::pair<unsigned int, int> > &winers,
+                        std::vector<std::pair<unsigned int, int> > &losers)
 {
 
 }
 
-bool PmergeMe::sort(void)
+std::vector<std::pair<unsigned int, int> > PmergeMe::sortVector(std::vector<std::pair<unsigned int, int> > &vec)
 {
-    
+    // special case for when vec.size == 1 from beginning
+    if(vec.size() <= 1)
+        return (vec);
+
+    std::vector<std::pair<unsigned int, int> > winers;
+    std::vector<std::pair<unsigned int, int> > losers;
+    //sort winers (going down):
+    fightVector(vec, winers, losers);
+    //recursive calls:
+    winers = sortVector(winers);
+    //sort losers (coming back):
+    jacobsthalVector(winers, losers);
+    return (winers);
+}
+
+void PmergeMe::FordJohnson(void)
+{
+    this->sortVector(this->_valuesV);
 }
 
 #ifdef DEBUG
