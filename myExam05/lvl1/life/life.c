647c94518b61173ee3b96309d79e11470833a06c
Raúl Manzanas Trillo <rmanzana@student.42barcelona.com>
2026-07-21 17:18:56 +0200

    life2.cero

--- myExam05/lvl1/life/life.c
+++ myExam05/lvl1/life/life.c
@@ -1,133 +1,129 @@
 #include "life.h"
 
-static void	print_map(t_game game)
+static void	init_game(t_game *game, char **argv)
 {
-	for (int y = 0; y < game.height; ++y)
+	game->width = atoi(argv[1]);
+	game->height = atoi(argv[2]);
+	game->iterations = atoi(argv[3]);
+
+	game->map = malloc(game->height * sizeof(char *));
+
+	for (int y = 0; y < game->height; y++)
 	{
-		for (int x = 0; x < game.width; ++x)
-			putchar(game.map[y][x]);
-		putchar('\n');
+		game->map[y] = malloc(game->width * sizeof(char));
+		for (int x = 0; x < game->width; x++)
+			game->map[y][x] = ' ';
 	}
 }
 
-static void	free_map(t_game *game)
-{
-	if (game->map)
-	{
-		for (int i = 0; i < game->height; ++i)
-			if (game->map[i])
-				free(game->map[i]);
-		free(game->map);
-	}
-}
-
-static void	fill_map(t_game *game)
+static void fill_map(t_game *game)
 {
 	char	buffer;
 	int		x = 0;
 	int		y = 0;
 	bool	draw = false;
-
+	
 	while (read(STDIN_FILENO, &buffer, 1) == 1)
 	{
-		switch (buffer)
+		switch(buffer)
 		{
-			case ('w'): if (y > 0) { --y; }; break ;
-			case ('a'): if (x > 0) { --x; }; break ;
-			case ('s'): if (y < game->height - 1) { ++y; } break ;
-			case ('d'): if (x < game->width - 1) { ++x; } break ;
-			case ('x'): draw = !draw; break ;
-			default: continue ;
+			case('w'): if (y > 0) y--; break;
+			case('s'): if (y < game->height - 1) y++; break;
+			case('a'): if (x > 0) x--; break;
+			case('d'): if (x < game->width - 1) x++; break;
+			case('x'): draw = !draw; break;
+			default: continue;
 		}
-
 		if (draw && x >= 0 && x < game->width && y >= 0 && y < game->height)
 			game->map[y][x] = '0';
 	}
 }
 
-static int	count_neighbors(t_game game, int y, int x)
+static int count_neighbors(t_game game, int y, int x)
 {
-	int	count = 0;
-
-	for (int dy = -1; dy < 2; ++dy)
+	int count = 0;
+	for (int ny = -1; ny < 2; ny++)
 	{
-		for (int dx = -1; dx < 2; ++dx)
+		for (int nx = -1; nx < 2; nx++)
 		{
-			if (dx == 0 && dy == 0)
-				continue ;
-
-			int	nx = x + dx;
-			int	ny = y + dy;
-			if (nx >= 0 && nx < game.width && ny >= 0 && ny < game.height && game.map[ny][nx] == '0')
+			if (nx == 0 && ny == 0)
+				continue;
+			
+			int ax = x + nx;
+			int ay = y + ny;
+			if (ax >= 0 && ax < game.width && ay >= 0 && ay < game.height && game.map[ay][ax] == '0')
 				count++;
 		}
 	}
-
-	return (count);
+	return count;
 }
 
-static void	play_game(t_game *game)
+static void free_map(t_game *game)
 {
-	char	**new_map = malloc(game->height * sizeof(char *));
-	for (int y = 0; y < game->height; ++y)
-		new_map[y] = malloc(game->width * sizeof(char *));
-
-	for (int y = 0; y < game->height; ++y)
+	if (game->map)
 	{
-		for (int x = 0; x < game->width; ++x)
+		for (int y = 0; y < game->height; y++)
+			if (game->map[y])
+				free(game->map[y]);
+		free(game->map);
+	}
+}
+
+static void print_map(t_game game)
+{
+	for (int y = 0; y < game.height; y++)
+	{
+		for (int x = 0; x < game.width; x++)
+			putchar(game.map[y][x]);
+		putchar('\n');
+	}
+}
+
+static void play_game(t_game *game)
+{
+	char **copy = malloc(game->height * sizeof(char *));
+	for (int y = 0; y < game->height; y++)
+		copy[y] = malloc(game->width * sizeof(char));
+
+	for (int y = 0; y < game->height; y++)
+	{
+		for (int x = 0; x < game->width; x++)
 		{
-			int	neighbors = count_neighbors(*game, y, x);
+			int nei = count_neighbors(*game, y, x);
 			if (game->map[y][x] == '0')
 			{
-				if (neighbors == 2 || neighbors == 3)
-					new_map[y][x] = '0';
+				if (nei == 2 || nei == 3)
+					copy[y][x] = '0';
 				else
-					new_map[y][x] = ' ';
+					copy[y][x] = ' ';
 			}
 			else
 			{
-				if (neighbors == 3)
-					new_map[y][x] = '0';
+				if (nei == 3)
+					copy[y][x] = '0';
 				else
-					new_map[y][x] = ' ';
-			}
+					copy[y][x] = ' ';
+			}	
 		}
 	}
 
 	free_map(game);
-	game->map = new_map;
+	game->map = copy;
 }
 
-static void	init_game(t_game *game, const char **argv)
-{
-	game->width = atoi(argv[1]);
-	game->height = atoi(argv[2]);
-	game->iteration = atoi(argv[3]);
-
-	game->map = malloc(game->height * sizeof(char *));
-	for (int y = 0; y < game->height; ++y)
-	{
-		game->map[y] = malloc(game->width * sizeof(char *));
-		for (int x = 0; x < game->width; ++x)
-			game->map[y][x] = ' ';
-	}
-}
-
-int	main(const int argc, const char **argv)
+int main(int argc, char *argv[])
 {
 	if (argc != 4)
-		return (1);
+		return 1;
 
-	t_game	game;
-
+	t_game game;
 	init_game(&game, argv);
 	fill_map(&game);
-
-	for (int i = 0; i < game.iteration; ++i)
+	for (int i = 0; i < game.iterations; i++)
 		play_game(&game);
 
 	print_map(game);
 	free_map(&game);
 
-	return (0);
+	return 0;
 }
--- myExam05/lvl1/life/life.h
+++ myExam05/lvl1/life/life.h
@@ -1,14 +1,14 @@
 #pragma once
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 typedef struct
 {
 	int		width;
 	int		height;
-	int		iteration;
+	int		iterations;
 	char	**map;
-} t_game;
+}	t_game;
